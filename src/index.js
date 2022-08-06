01631e60117347a13a853afff85398816b019af7
talasago <42575556+talasago@users.noreply.github.com>
2022-08-06 16:31:59 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+class Search extends React.Component {
+  render (){
+    return ("hogehoge");
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='main'>
+        <div className='header'>
+          <h2>ふりかえり手法を決めてくれるやつ</h2>
+        </div>
+        <div className='search'>
+          <Search />
+        </div>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
