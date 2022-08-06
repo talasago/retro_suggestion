4113bff44c0cdfea7e825875438b0b433ebd24b6
talasago <42575556+talasago@users.noreply.github.com>
2022-08-06 23:32:20 +0900

    feat: Modify checkbox to map

--- src/index.js
+++ src/index.js
@@ -1,77 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import AppHeader from './component/AppHeader';
-import { Container } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppHeader from "./component/AppHeader";
+import { Container } from "@mui/material";
 
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Checkbox from '@mui/material/Checkbox';
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Checkbox from "@mui/material/Checkbox";
 
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
 
-import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
+import retrospectiveData from "./retrospective.json";
+import retrospectivePurposeName from "./retrospective_purpose_name.json";
 
 class Search extends React.Component {
   render() {
     return (
       <>
-      <div>
-        <SearchCheckBoxs />
-      </div>
-      <div>
-        <Button variant="contained" startIcon={<SearchIcon />}>
-          検索
-        </Button>
-      </div>
+        <div>
+          <SearchCheckBox />
+        </div>
+        <div>
+          <Button variant="contained" startIcon={<SearchIcon />}>
+            検索
+          </Button>
+        </div>
       </>
     );
   }
 }
 
-class SearchCheckBoxs extends React.Component {
-  //チェックボックスの内容の状態をもつ
-  //TODO:目的をmapとかにする。
-  //クラスに複数形はだめかなあ
+class SearchCheckBox extends React.Component {
+  //TODO:チェックボックスの内容の状態をもつ
 
   render() {
+    const checkBoxes = Object.entries(retrospectivePurposeName).map(
+      ([num, name]) => {
+        return (
+          <FormControlLabel
+            control={<Checkbox />}
+            label={name}
+            labelPlacement="start"
+          />
+        );
+      }
+    );
+
     return (
-        <FormControl component="fieldset">
+      <FormControl component="fieldset">
         <FormLabel component="legend">ふりかえり目的</FormLabel>
         <FormGroup aria-label="position" row>
-          <FormControlLabel
-            control={<Checkbox />}
-            label="目的1"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="目的2"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="目的3"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="目的4"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            control={<Checkbox />}
-            label="目的5"
-            labelPlacement="start"
-          />
+          {checkBoxes}
         </FormGroup>
       </FormControl>
     );
   }
 }
-
 
 class App extends React.Component {
   render() {
@@ -82,11 +69,9 @@ class App extends React.Component {
           <Search />
         </Container>
       </div>
-    )
+    );
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
