4c90a5e9ababc285ba9a53c216462b26615ddbdf
talasago <42575556+talasago@users.noreply.github.com>
2022-08-10 17:10:11 +0900

    feat: add CFn

---
+++ create_stack.bash
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd "$(dirname "${0}")" || exit
+
+readonly STACK_NAME="retro-suggestion"
+aws cloudformation create-stack \
+    --stack-name ${STACK_NAME} \
+    --template-body file://./infra/s3.yml \
+
+aws cloudformation wait stack-create-complete \
+    --stack-name ${STACK_NAME}
